d8f2b0272f2f239d731d5b61689e2235d6e4f412
domanh09012006 <doducmanh09012006@gmail.com>
2026-03-11 22:44:13 +0700

    exception

--- src/com/fastfood/exception/InsufficientStockException.java
+++ src/com/fastfood/exception/InsufficientStockException.java
@@ -1,4 +1,8 @@
 package com.fastfood.exception;
 
-public class InsufficientStockException {
+// Dùng khi khách hàng đặt số lượng món vượt quá số lượng còn lại trong kho.
+public class InsufficientStockException extends RuntimeException {
+    public InsufficientStockException(String message) {
+        super(message);
+    }
 }
--- src/com/fastfood/exception/InvalidOrderIdException.java
+++ src/com/fastfood/exception/InvalidOrderIdException.java
@@ -1,4 +1,8 @@
 package com.fastfood.exception;
 
-public class InvalidOrderIdException {
+//Dùng khi tìm kiếm hoặc cập nhật một đơn hàng có ID không tồn tại trong OrderRepository.
+public class InvalidOrderIdException extends RuntimeException {
+    public InvalidOrderIdException(String message) {
+        super(message);
+    }
 }
