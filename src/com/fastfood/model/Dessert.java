21b90953b5f63c93be65ec08a83a84d5e4dba6f7
Ngô Trung Chiến <ngotrungchien232@gmail.com>
2026-03-11 23:36:40 +0700

    feature/drink/dessert

---
+++ src/com/fastfood/model/Dessert.java
@@ -0,0 +1,22 @@
+package com.fastfood.model;
+
+public class Dessert extends MenuItem {
+
+    public Dessert(int id, String name, double price) {
+        super(id, name, price);
+    }
+
+    @Override
+    public double caculatePrice() {
+        // Mon trang mieng tinh gia goc, khong tinh thue
+        return getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " | Loai: Trang mieng" +
+                " | Gia tinh: " + String.format("%.2f", caculatePrice());
+    }
+}
+
--- src/com/fastfood/model/Drink.java
+++ src/com/fastfood/model/Drink.java
@@ -1,4 +1,39 @@
 package com.fastfood.model;
 
-public class Drink {
+public class Drink extends MenuItem {
+    // Kich thuoc: S, M, L
+    private String size;
+
+    public Drink(int id, String name, double price, String size) {
+        super(id, name, price);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    @Override
+    public double caculatePrice() {
+        // Tinh gia theo size: S giu nguyen, M tang 20%, L tang 50%
+        switch (size.toUpperCase()) {
+            case "M":
+                return getPrice() * 1.2;
+            case "L":
+                return getPrice() * 1.5;
+            default: // Size S hoac mac dinh
+                return getPrice();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " | Size: " + size +
+                " | Gia tinh: " + String.format("%.2f", caculatePrice());
+    }
 }
